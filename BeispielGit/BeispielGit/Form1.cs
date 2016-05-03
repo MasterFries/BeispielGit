772fed37f88812bb547e538184d1570ea55510d2
Andrej Friesen <andrej.friesen@gmx.de>
2016-05-03 11:24:38 +0200

    - Eine dynamische Textbox erstellt

---
+++ BeispielGit/BeispielGit/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeispielGit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            createTextBox();
+        }
+
+        private void createTextBox()
+        {
+            TextBox box = new TextBox();
+            box.Width = 150;
+            box.Height = 50;
+            box.Top = 30;
+            box.Left = 50;
+            Controls.Add(box);
+        }
+    }
+}
